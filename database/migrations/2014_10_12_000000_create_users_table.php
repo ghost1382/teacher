35d06aecfdbf5519bb802ed62bbafae293f9ff9b
ghost1382 <mohammed.bn884@gmail.com>
2023-03-19 18:34:47 +0100

    AddTheClasse Module&Table&ChangedTheclass_id-fkey

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
@@ -20,19 +15,19 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('class_id')->nullable();
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
             $table->foreignId('user_role_id')->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['class_id']);
+        });
+
         Schema::dropIfExists('users');
     }
 }
